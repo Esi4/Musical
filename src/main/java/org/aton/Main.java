c8867c0966c58e4d3dbaee6c109059c3e1504d3e
Esi4 <kolupaeff13102001@yandex.ru>
2024-05-29 23:13:14 +0300

    optimized

--- src/main/java/org/aton/Main.java
+++ src/main/java/org/aton/Main.java
@@ -3,14 +3,12 @@ package org.aton;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        testControlReplica();
-        //testScenario();
-    }
+    public static final String PATH_TO_SCENARIO = "C:\\Users\\Tyauch\\Desktop\\Aton quests\\Musical\\Musical\\src\\main\\resources\\scenario";
 
-    public static void testControlReplica() {
-        ControlReplica controlReplica = new ControlReplica();
-        controlReplica.scene();
+    public static void main(String[] args) throws InterruptedException {
+        ScenarioParser scenarioParser = new ScenarioParser(PATH_TO_SCENARIO);
+        ControlScene controlScene = new ControlScene(scenarioParser);
+        controlScene.scene();
     }
 
     public static void testScenario() {
