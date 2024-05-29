45b1f164bfe07bd5765b625019b54d32674d6060
Esi4 <kolupaeff13102001@yandex.ru>
2024-05-29 23:12:33 +0300

    add tests

---
+++ src/test/java/TestControlScene.java
@@ -0,0 +1,39 @@
+import org.assertj.core.api.Assertions;
+import org.aton.ControlScene;
+import org.aton.ScenarioParser;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestControlScene {
+    public static final String PATH_TO_SCENARIO = "C:\\Users\\Tyauch\\Desktop\\Aton quests\\Musical\\Musical\\src\\main\\resources\\scenario";
+
+    @Test
+    void testScene() throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        PrintStream customOut = new PrintStream(byteArrayOutputStream);
+        ScenarioParser scenarioParser = new ScenarioParser(PATH_TO_SCENARIO);
+        ControlScene controlScene = new ControlScene(scenarioParser);
+        List<String> expectedNameActors = Files.readAllLines(Paths.get(PATH_TO_SCENARIO));
+
+        System.setOut(customOut);
+
+        try {
+            controlScene.scene();
+        } finally {
+            System.setOut(originalOut);
+        }
+
+        String output = byteArrayOutputStream.toString();
+        List<String> outputLines = new ArrayList<>(Arrays.asList(output.split(System.lineSeparator())));
+        Assertions.assertThat(outputLines).containsExactlyElementsOf(expectedNameActors);
+    }
+}
---
+++ src/test/java/TestScenarioParser.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+import org.assertj.core.api.Assertions;
+import org.aton.Actor;
+import org.aton.ScenarioParser;
+import org.junit.jupiter.api.Test;
+
+
+public class TestScenarioParser {
+    public static final String PATH_TO_SCENARIO = "C:\\Users\\Tyauch\\Desktop\\Aton quests\\Musical\\Musical\\src\\main\\resources\\scenario";
+    public static final String PATH_TO_TEST_ACTORS = "C:\\Users\\Tyauch\\Desktop\\Aton quests\\Musical\\Musical\\src\\main\\resources\\Test_Actors";
+    public static final String PATH_TO_TEST_SCENARIO = "C:\\Users\\Tyauch\\Desktop\\Aton quests\\Musical\\Musical\\src\\main\\resources\\Test_Scenario";
+    private List<String> expectedNameActors;
+    private Queue<String> expectedScenario;
+
+    public void preparingExpectedScenario() throws IOException {
+        expectedScenario = new LinkedBlockingDeque<>();
+        List<String> lines = Files.readAllLines(Paths.get(PATH_TO_TEST_SCENARIO));
+        expectedScenario.addAll(lines);
+
+    }
+
+    public void preparingExpectedActorsName() throws IOException {
+        expectedNameActors = Files.readAllLines(Paths.get(PATH_TO_TEST_ACTORS));
+    }
+
+    @Test
+    void parseActorsNameTest() throws IOException {
+        ScenarioParser scenarioParser = new ScenarioParser(PATH_TO_SCENARIO);
+        List<Actor> resultActors = scenarioParser.getActors();
+        List<String> resultNameActors = new ArrayList<>();
+        for(Actor actor: resultActors) {resultNameActors.add(actor.getName());}
+
+        preparingExpectedActorsName();
+
+        Assertions.assertThat(resultNameActors).containsExactlyElementsOf(expectedNameActors);
+    }
+
+    @Test
+    void parseScenarioTest() throws IOException {
+        ScenarioParser scenarioParser = new ScenarioParser(PATH_TO_SCENARIO);
+        Queue<String> resultScenario = scenarioParser.getScenario();
+        preparingExpectedScenario();
+
+        Assertions.assertThat(resultScenario).containsExactlyElementsOf(expectedScenario);
+    }
+}
